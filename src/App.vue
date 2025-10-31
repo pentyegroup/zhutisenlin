3ff75e7c8995ac2f7c7db089c61105cf48126459
pentyegroup <it@pentyegroup.com>
2025-10-31 15:14:03 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@ const handleActiveDevice = (device: Device) => {
 
 // 网址处理
 const protocol = ref("https"); // 默认协议为 https
-const url = ref("baiwumm.com");
+const url = ref("themeforest.net");
 const phone = ref("");
 const pad = ref("");
 const computer = ref("");
